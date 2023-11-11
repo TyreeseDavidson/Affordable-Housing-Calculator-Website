01fdc6be14c03c5fde84383560745cdfcba58b48
TyreeseDavidson <tyreesedavidson00@gmail.com>
2023-11-11 18:55:49 -0500

    changes to have options for emotions

--- affirmation_section.js
+++ affirmation_section.js
@@ -1,16 +1,39 @@
-const affirmations = [
-    "I am confident and capable.",
-    "I radiate love and positivity.",
-    "I am in control of my thoughts and emotions.",
-    "I attract success and abundance into my life.",
-    "I am worthy of happiness and success.",
-    "I am surrounded by positivity and love.",
-    "I am a magnet for positive energy.",
-    "I believe in my abilities and express my true self with ease.",
-];
+const emotions = {
+    happy: [
+      "I am filled with joy and gratitude.",
+      "Happiness is my natural state.",
+      "I radiate positivity and attract joy."
+    ],
+    calm: [
+      "I am at peace with myself and the world.",
+      "I breathe in calmness and breathe out stress.",
+      "I am centered and grounded."
+    ],
+    motivated: [
+      "I am motivated and ready to take on challenges.",
+      "I am focused on my goals and determined to achieve them.",
+      "I have the power to create change in my life."
+    ],
+    // emotion[],
+    // emotion[],
+    // emotion[],
+    // emotion[],
+    // emotion[],
+    // emotion[],
+    // emotion[],
+    // emotion[],
+  };
 
 function generateAffirmation() {
-    const randomIndex = Math.floor(Math.random() * affirmations.length);
-    const affirmationElement = document.getElementById('affirmation');
-    affirmationElement.textContent = affirmations[randomIndex];
+    const selectedEmotion = document.getElementById('emotionSelect').value;
+    const affirmationContainer = document.getElementById('affirmation');
+  
+    if (emotions[selectedEmotion]) {
+      const randomIndex = Math.floor(Math.random() * emotions[selectedEmotion].length);
+      const randomAffirmation = emotions[selectedEmotion][randomIndex];
+      affirmationContainer.textContent = randomAffirmation;
+    } else {
+      affirmationContainer.textContent = "Please select an emotion.";
+    }
+  
 }
