9835749dc8860143565104faa622c76c30ef85f0
TyreeseDavidson <tyreesedavidson00@gmail.com>
2023-11-12 09:42:39 -0500

    adding emotions options

--- affirmation_section.js
+++ affirmation_section.js
@@ -1,14 +1,5 @@
 const emotions = {
-    happy: [
-      "I am filled with joy and gratitude.",
-      "Happiness is my natural state.",
-      "I radiate positivity and attract joy."
-    ],
-    calm: [
-      "I am at peace with myself and the world.",
-      "I breathe in calmness and breathe out stress.",
-      "I am centered and grounded."
-    ],
+    
     Burnout: {
         affirmations:[
             "I don’t compare myself to others. The only person I compare myself to is the person I was yesterday. And as long as the person I am today is even the tiniest bit better than the person I was yesterday—I’m meeting my own definition of success.",
@@ -41,15 +32,253 @@ const emotions = {
             "Go to Dorney Park for a day"
             ]
           },
-    // emotion[],
-    // emotion[],
-    // emotion[],
-    // emotion[],
-    // emotion[],
-    // emotion[],
-    // emotion[],
-    // emotion[],
-  };
+          IsolatedLonely: {
+            affirmations: [
+              "Loneliness is just a feeling, and feelings can change. I'm giving myself the time I need.",
+              "I may not have many people around me at the moment, but I can still connect with others when the time is right.",
+              "It's okay to feel lonely; it doesn't mean I'm unloved or unwanted.",
+              "I may be alone, but I'm not the only one feeling this way. I'm not truly isolated in my experiences.",
+              "Even if I'm physically alone, I can still find connection through shared interests and online communities.",
+              "My worth isn't determined by the number of people in my immediate circle; I am still valuable.",
+              "I have the power to take small steps to connect with others when I'm comfortable.",
+              "Being lonely right now doesn't mean I'll always feel this way. Tomorrow is a new opportunity to reach out.",
+              "I am worthy of forming meaningful connections, and it's okay to start small.",
+              "Quality matters more than quantity. A few genuine connections can make a big difference.",
+              "I attract understanding people, and I can choose to share my thoughts and feelings when I'm ready.",
+              "Solitude allows me to discover more about myself, and that's a journey worth taking.",
+              "Even if it's just me right now, I am part of a larger world with opportunities for connection.",
+              "I have the power to radiate positive energy, and that can draw others toward me.",
+              "Building a supportive community takes time, and I can be patient with the process.",
+              "I am open to the idea that meaningful connections can happen, even if it takes time.",
+              "I may feel lonely now, but I'm not closing myself off to the possibility of new friendships."
+            ],
+            activties: [
+              "Join the Bike and Barre Class at LVHN Fitness",
+              "Help volunteer in a group you're passionate about. You can find a volunteer opportunity by going online to the volunteer center of the Lehigh Valley.",
+              "Start your own club or organization based on something that interests you.",
+              "Try the Paint Some Pottery Workshop",
+              "Try the Nazareth Ceramic Center Workshop",
+              "Join the Lehigh Valley Adult Sports League",
+              "Connect with a NAMI support group",
+              "Join a nature group traveling to a site you're interested in",
+              "Join the Lehigh Valley Mall Social Walk",
+              "Attend a Lehigh Valley Phantoms Hockey Game",
+              "Attend the Lehigh Valley Food Truck Festival"
+            ]
+          },
+          LowSelfEsteem: {
+            affirmations: [
+              "I am confident in my unique qualities and talents.",
+              "I am enough, just as I am.",
+              "I embrace my imperfections; they make me who I am.",
+              "I am valuable and worthy of respect from others.",
+              "I deserve to be treated with kindness and compassion.",
+              "I trust that I have the strength to overcome challenges.",
+              "I am becoming a better version of myself every day.",
+              "I accept compliments graciously and believe in their sincerity.",
+              "I choose self-love and self-acceptance over self-criticism.",
+              "I celebrate my uniqueness and embrace my individuality.",
+              "I am a work in progress, and that's okay.",
+              "I am proud of who I am and continue to become.",
+              "I am constantly growing and evolving into a better version of myself.",
+              "I am in control of my thoughts, and I choose positive and uplifting ones.",
+              "I forgive myself for past mistakes and learn from them."
+            ],
+            activties: [
+              "Attend Culinary Classes at Easton Public Market",
+              "Try the Lehigh Valley Roller Derby",
+              "Hiking at Jacobsburg Environmental Education Center",
+              "Take a yoga class at The Yoga Loft of Bethlehem",
+              "Meditate at High Vibration Wellness in Allentown",
+              "Visit the Lehigh Valley Heritage Museum",
+              "Take a stroll along the Karl Stirner Arts Trail",
+              "Attend and meet new people at Musikfest",
+              "Explore the Banana Factory Arts Center",
+              "Explore the SouthSide Film Festival in Bethlehem"
+            ]
+          },
+          
+          Anxiety: {
+            affirmations: [
+              "I am surrounded by positive energy and loving people.",
+              "I am grounded, and my mind is clear and focused.",
+              "I am safe, and this feeling of anxiety will pass.",
+              "I trust the wisdom of my body and mind to guide me through.",
+              "I let go of fear and embrace the present moment with courage.",
+              "I am worthy of peace, and I allow it to flow through me.",
+              "I am not my anxiety; I am a person with strengths and abilities.",
+              "I am capable of finding joy and serenity in the midst of uncertainty.",
+              "I am in control of my breath, and I inhale calmness, exhale tension.",
+              "I release the need to control everything and trust in the process of life.",
+              "I am safe at this moment; my anxiety does not define me.",
+              "I choose peace over worry and trust that everything will work out.",
+              "I am stronger than my anxious thoughts, and I can overcome them.",
+              "I acknowledge my feelings without judgment and let them pass.",
+              "I invite calmness and tranquility into my mind and body."
+            ],
+            activties: [
+              "Take a yoga class at Easton Yoga",
+              "Explore the services at the Lehigh Valley Healing Arts Center",
+              "Try Mindfulness Meditation Group at the Reiki School + Clinic",
+              "Take a walk along the Delaware and Lehigh National Heritage Corridor Trail",
+              "Try Therapeutic Horseback Riding at Equi-librium",
+              "Spend time at the Lehigh Valley Zoo",
+              "Visit the Lehigh Valley Acupuncture and Wellness Center",
+              "Take advantage of the Lehigh Valley Mall Relaxation Zones",
+              "Take a Cultural Exploration at the Allentown Art museum",
+              "Relax at the Lehigh Valley Botanical Society Gardens",
+              "Try the Lehigh Valley Parkour and Movement Arts",
+              "Visit Lehigh Valley Farmers Markets (Ex: Easton Farmers Market)",
+              "Visit Horses Unplugged LLC"
+            ]
+          },
+          
+          Guilt: {
+            affirmations: [
+              "I take responsibility for my actions and commit to making amends.",
+              "I release the need for perfection and accept myself with all my imperfections.",
+              "I am learning and evolving, and I give myself permission to do so.",
+              "I choose self-compassion over self-condemnation.",
+              "I am allowed to move forward without carrying the weight of guilt.",
+              "I release the past and focus on creating a positive future.",
+              "I am a good person, capable of making amends and growing from my experiences.",
+              "I forgive myself and let go of the guilt that no longer serves me.",
+              "I choose to treat myself with kindness and understanding.",
+              "I am on a journey of self-improvement, and each step forward is progress.",
+              "I acknowledge my mistakes without judgment and learn from them.",
+              "I am human, and making mistakes is a natural part of life.",
+              "I release the burden of guilt and forgive myself for not being perfect.",
+              "I choose to focus on the lessons learned from my experiences.",
+              "I am allowed to grow and evolve; my past does not define me.",
+              "I am worthy of self-compassion and forgiveness.",
+              "I let go of guilt and embrace the opportunity for positive change."
+            ],
+            activties: [
+              "Try the Lehigh Valley Roller Derby",
+              "Hiking at Jacobsburg Environmental Education Center",
+              "Take a yoga class at The Yoga Loft of Bethlehem",
+              "Meditate at High Vibration Wellness in Allentown",
+              "Visit the Lehigh Valley Heritage Museum",
+              "Take a stroll along the Karl Stirner Arts Trail",
+              "Attend and meet new people at Musikfest",
+              "Explore the Banana Factory Arts Center",
+              "Explore the SouthSide Film Festival in Bethlehem"
+            
+            ] 
+          },
+          Overwhelmed: {
+            affirmations: [
+              "I am capable of handling whatever comes my way.",
+              "I release the need to control everything and trust the process of life.",
+              "I breathe deeply, inhaling peace and exhaling tension.",
+              "I prioritize my well-being and take one step at a time.",
+              "I ask for help when needed, and that is a sign of strength, not weakness.",
+              "I choose to focus on what I can control and let go of what I cannot.",
+              "I trust in my ability to find solutions and overcome challenges.",
+              "I give myself permission to take a break and recharge.",
+              "I am not alone; support is available to me.",
+              "I release the need for perfection and accept that I am doing my best.",
+              "I break down my tasks into manageable steps and take them one at a time.",
+              "I am resilient, and this moment of overwhelm will pass.",
+              "I am in control of my thoughts, and I choose positivity.",
+              "I trust myself to make decisions that are right for me.",
+              "I am allowed to prioritize my own needs and set boundaries.",
+              "I release the pressure to please everyone and focus on what matters most.",
+              "I choose to approach challenges with a calm and clear mind.",
+              "I acknowledge my feelings without judgment and give myself the space to process.",
+              "I am strong, and I can navigate through overwhelming situations.",
+              "I am deserving of peace, and I create it within myself."
+            ],
+            activties: [
+              "Get a massage at Healing Hands Massage and Wellness Center",
+              "Talk to someone at Thriveworks Counseling & Psychiatry Allentown",
+              "Disconnect from technology",
+              "Go climbing at North Summit Climbing Gym",
+              "Go to Nurture Nature Center in Easton",
+              "Visit Horses Unplugged LLC",
+              "Try Mindfulness Meditation Group at the Reiki School + Clinic",
+              "Meditate at High Vibration Wellness in Allentown",
+              "Take a break and get a sweet treat at The Cup",
+              "Take advantage of Live Music Sundays! Hosted by Folino Estate"
+            ]
+          },
+          
+          Doubt: {
+            affirmations: [
+              "I believe in my abilities and trust the path that I am on.",
+              "I release the need for certainty and embrace the beauty of the unknown.",
+              "I am capable and resilient, even in the face of uncertainty.",
+              "I trust myself to make decisions that align with my values and goals.",
+              "I am deserving of success, and I have the skills to achieve it.",
+              "I let go of the fear of failure; mistakes are opportunities to learn and grow.",
+              "I am constantly evolving, and my potential is limitless.",
+              "I replace self-doubt with confidence and faith in my abilities.",
+              "I am not defined by my past; I have the power to shape my future.",
+              "I choose self-encouragement over self-criticism.",
+              "I release the need for perfection and embrace my authentic self.",
+              "I am worthy of success, and I allow myself to pursue my dreams.",
+              "I trust the wisdom of my intuition; it guides me in the right direction.",
+              "I am open to new possibilities and trust the timing of my life.",
+              "I acknowledge my progress and celebrate my achievements.",
+              "I have the power to overcome challenges and turn them into opportunities.",
+              "I am resilient, and setbacks are just stepping stones to success.",
+              "I let go of comparing myself to others and focus on my unique journey.",
+              "I trust in my own judgment and decisions.",
+              "I am enough, just as I am, and I deserve to believe in myself."
+            ],
+            activties: [
+              "Talk to someone at Thriveworks Counseling & Psychiatry Allentown",
+              "Take a walk at Delaware & Lehigh National Heritage Corridor",
+              "Visit Horses Unplugged LLC",
+              "Join the Lehigh Valley Mall Social Walk",
+              "Disconnect from technology",
+              "Go climbing at North Summit Climbing Gym",
+              "Go to Nurture Nature Center in Easton",
+              "Try Mindfulness Meditation Group at the Reiki School + Clinic",
+              "Meditate at High Vibration Wellness in Allentown",
+              "Take a break and get a sweet treat at The Cup",
+              "Take advantage of Live Music Sundays! Hosted by Folino Estate"
+            
+            ]
+          },
+          
+          Paranoia: {
+            affirmations: [
+              "I am safe and secure in this moment.",
+              "I choose to focus on the facts rather than my fears.",
+              "I trust myself to distinguish between reality and anxious thoughts.",
+              "I release the need to anticipate negative outcomes.",
+              "I am in control of my thoughts, and I choose to let go of paranoia.",
+              "I am aware that my thoughts do not define my reality.",
+              "I release the need to constantly watch for potential threats.",
+              "I choose to live in the present and let go of catastrophic thinking.",
+              "I am surrounded by love and support.",
+              "I challenge irrational thoughts and replace them with realistic ones.",
+              "I trust the people in my life, and I am open to building positive connections.",
+              "I release the grip of fear, allowing space for peace and calm.",
+              "I am resilient and can cope with uncertainty without succumbing to paranoia.",
+              "I focus on the evidence rather than assumptions.",
+              "I am grounded, and my mind is clear and rational.",
+              "I deserve to experience life without the burden of constant suspicion.",
+              "I am not alone, and it's okay to seek support.",
+              "I choose to engage in activities that bring me joy and relaxation.",
+              "I am capable of managing my thoughts and emotions.",
+              "I am on a journey of healing and growth, leaving behind unnecessary fears."
+            ],
+            activties: [
+              "Talk to someone at Thriveworks Counseling & Psychiatry",
+              "Take a walk at Delaware & Lehigh National Heritage Corridor",
+              "Visit Horses Unplugged LLC",
+              "Join the Lehigh Valley Mall Social Walk",
+              "Disconnect from technology"
+
+            ]
+          
+          },
+  }
+
+
+
 
 
   function generateAffirmation() {
